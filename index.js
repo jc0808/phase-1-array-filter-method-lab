0fd234b948621171c7d5e923813334b03dfd7d63
Joshua Campos <jdeveloper1203@gmail.com>
2022-08-30 17:20:03 -0400

    commit tree bca9754165f07d3bf0f9a927f6282c9f64caba1a

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+function findMatching(array, string){
+    return array.filter(element => {
+
+        if (element.toLowerCase() == string.toLowerCase()){
+            return element;
+        }
+    });
+};
+
+function fuzzyMatch(array, string){
+    return array.filter(element => {
+
+        if (element.toLowerCase().charAt(0) == string.toLowerCase().charAt(0) && 
+        element.toLowerCase().charAt(1) == string.toLowerCase().charAt(1) || 
+        element.toLowerCase().charAt(0) == string.toLowerCase().charAt(0)){
+            return element;
+        }
+    });
+}
+
+function matchName(array, string){
+    return array.filter(element => {
+        if (element.name === string){
+            return element;
+        }
+    })
+}
